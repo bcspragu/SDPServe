407ef71f49ae873be3041138be3ebf77a1948c23
Brandon Sprague <brandon00sprague@gmail.com>
2014-09-10 12:07:39 -0400

    First commit

---
+++ main.go
@@ -0,0 +1,58 @@
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+  "encoding/json"
+	"flag"
+	"log"
+	"net/http"
+	"text/template"
+)
+
+const (
+  XSize = 10
+  YSize = 10
+)
+
+var addr = flag.String("addr", ":8080", "http service address")
+var homeTempl = template.Must(template.ParseFiles("home.html"))
+var grid [XSize][YSize]bool;
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", 404)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+  gridString, _ := json.Marshal(grid)
+  data := struct {
+    XCount int
+    YCount int
+    Host string
+    Grid string
+  } {
+    XSize,
+    YSize,
+    r.Host,
+    string(gridString),
+  }
+	homeTempl.Execute(w, data)
+}
+
+func main() {
+	flag.Parse()
+	go h.run()
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", serveWs)
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
