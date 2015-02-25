f5b2adb49df40a14aecfa9dbae95102a3d673a92
dpbond <bond.daniel.p@gmail.com>
2015-02-25 13:29:22 -0500

    Changed Instrument object references to GenericPlayer

--- danny/MusicPlayer.java
+++ danny/MusicPlayer.java
@@ -6,8 +6,8 @@ import javax.sound.midi.MidiSystem;
 public class MusicPlayer {
   public static void main(String args[]) throws Exception {
     MidiChannel[] channels = MidiSystem.getSynthesizer().getChannels();
-    Instrument piano = new Instrument(channels, new int[]{0,1,2,3});
-    Instrument guitar = new Instrument(channels, new int[]{4,5,6,7});
+    GenericPlayer piano = new GenericPlayer(channels, new int[]{0,1,2,3});
+    GenericPlayer guitar = new GenericPlayer(channels, new int[]{4,5,6,7});
   }
 }
 
