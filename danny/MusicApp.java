0a3ec43fe9fe8654cba7b09b56ec67ac8befa984
dpbond <bond.daniel.p@gmail.com>
2015-02-25 14:35:55 -0500

    Update MusicApp.java
    Updated main with a brief test of GenericPlayers

--- danny/MusicApp.java
+++ danny/MusicApp.java
@@ -7,16 +7,21 @@ public class MusicApp {
 	
 	public static MidiChannel[] channels;
 	
+	public static int columnHeight = 12;
+	public static int rowLength = 12;
+	
 	//Declare arrays for instrument grids
-	int[][] percussion = new int[12][12];
-	int[][] piano = new int[12][12];
-	int[][] instrument1 = new int[12][12];
-	int[][] instrument2 = new int[12][12];
+	public static boolean[][] percussion1 = new boolean[rowLength][columnHeight];
+	public static boolean[][] percussion2 = new boolean[rowLength][columnHeight];
+	public static boolean[][] piano = new boolean[rowLength][columnHeight];
+	public static boolean[][] guitar = new boolean[rowLength][columnHeight];
+	
+	
 
 	public static void main( String[] args ) throws MidiUnavailableException, InvalidMidiDataException {
 		
 		//Instantiate necessary music creating objects and variables
-		int volume = 100; 					// between 0 and 127
+		int velocity = 100; 					// between 0 and 127
 		int duration = 1000; 				// in milliseconds
 		
 		try {
@@ -29,20 +34,20 @@ public class MusicApp {
 			Instrument inst[] = synth.getLoadedInstruments();		//Acquire an array of all instruments in this sound bank
 			
 			/*Instrument Setup*/
-			Instrument piano = inst[0];			//Piano 1
-			Instrument guitar = inst[28];		//Clean Gt.
+			Instrument pianoInst = inst[0];			//Piano 1
+			Instrument guitarInst = inst[28];		//Clean Gt.
 //			Instrument bongo = null;			//Haven't decided on instrument yet
 //			Instrument cymbal = null;			//Haven't decided on instrument yet
 			boolean isSetupSuccess = false;
-			isSetupSuccess = setInstruments(piano, guitar);
+			isSetupSuccess = setInstruments(pianoInst, guitarInst);
 			
 			if (!isSetupSuccess) {
 				System.out.println("Instrument setup failed!");	//This represents an instrument setup failure
 				System.exit(0);
 			}
 			else {
-				Patch pianoPatch = piano.getPatch();				//Retrieves sound bank and program number
-				Patch guitarPatch = guitar.getPatch();
+				Patch pianoPatch = pianoInst.getPatch();				//Retrieves sound bank and program number
+				Patch guitarPatch = guitarInst.getPatch();
 				
 				/*Four Channels for Piano Notes*/
 				channels[0].programChange(pianoPatch.getBank(), pianoPatch.getProgram());
@@ -59,20 +64,21 @@ public class MusicApp {
 				/*Main Program Loop*/
 //				while (true) {
 //					//updateGrids();			//We want latest grid implementation. Do this with Brandon
-//					
-//					//Object method for interpreting Grid1
-//					//Object method for interpreting Grid2
-//					//Object method for interpreting Grid3
-//					//Object method for interpreting Grid4
-//					
-//					//Verify all tracks equal length. Important because game is periodic sweep of the grids.
-//					
-//					//Prepare 12 Note Sequences. Methods will contain noteOn events, but no sleeps or note offs. All channels will have noteOff events applied by whatever the universal kill method is
 //				}
 				/*End of Main Program Loop*/
-//				
+
+				
 				/*Test Block*/
-				playNotes(new int[]{48,60,64,67}, volume, duration, channels);
+				GenericPlayer p1 = new GenericPlayer(channels, new int[]{0,1,2,3});
+			    GenericPlayer g1 = new GenericPlayer(channels, new int[]{4,5,6,7});
+			    
+			    System.out.println("About to play a chord");
+			    
+			    p1.playChord("CMajor", "C", velocity, duration);
+			    //g1.playChord("CMajor", "em", velocity, duration);
+			    
+			    System.out.println("Played the chords");
+				
 				/*End of Test Block*/
 				
 				synth.close();
@@ -84,6 +90,7 @@ public class MusicApp {
 		}
 		
 	}
+	
 	
 	public static boolean updateGrids() {
 		//fill in later, for loops to update each value
@@ -99,33 +106,32 @@ public class MusicApp {
 	public static boolean setInstruments(Instrument inst1, Instrument inst2) {
 		return inst1 != null && inst2 != null;
 	}
-
-	public static void playNotes(int notes[], int velocity, int duration, MidiChannel[] chans) throws InterruptedException {
-			/*Experiment method when testing different channels. Remove in final copy*/
+	
+	public static String determineMajorKey() {
+		//First column of piano grid will determine Major scale of our grids
+		String key = null;
+		int counter = 0;
+		for (int i = 0; i < columnHeight; i++) {
+			if (piano[0][i]) {			//If this cell contains a true, increment our counter
+				counter++;
+			}
+		}
 		
-			//Piano
-//			chans[0].noteOn( notes[0], velocity );
-//			chans[1].noteOn( notes[1], velocity );
-//			chans[2].noteOn( notes[2], velocity );
-//			chans[3].noteOn( notes[3], velocity );
-//			Thread.sleep( duration );
-			//Guitar
-//			chans[4].noteOn( 50, velocity );
-//			chans[5].noteOn( 57, velocity );
-//			chans[6].noteOn( 62, velocity );
-//			chans[7].noteOn( 65, velocity );
-//			Thread.sleep( duration );
-			//Drums
-			chans[9].noteOn(70, velocity);
-			chans[9].noteOff(70, velocity);
-			chans[9].noteOn(43, velocity);
-			chans[9].noteOff(43, velocity);
-			Thread.sleep(duration);
-//			channels[0].noteOff( notes[0] );
-//			channels[1].noteOff( notes[1] );
-//			channels[2].noteOff( notes[2] );
-//			channels[3].noteOff( notes[3] );
+		if (counter == 0 || counter == 4 || counter == 8) {
+			key = "CMajor";
+		}
+		else if (counter == 1 || counter == 5 || counter == 9) {
+			key = "GMajor";
+		} 
+		else if (counter == 2 || counter == 6 || counter == 10) {
+			key = "DMajor";
+		}
+		else if (counter == 3 || counter == 7 || counter == 11) {
+			key = "FMajor";
+		}
+		return key;
 	}
+		
 	
 }
 
