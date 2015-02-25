6845a75baee403eaa892bcff4bc132c1b6392d52
dpbond <bond.daniel.p@gmail.com>
2015-02-25 15:49:24 -0500

    A slightly more advanced progression
    Guitar soundbank isn't set in here, so it's two pianos

--- danny/MusicPlayer.java
+++ danny/MusicPlayer.java
@@ -14,7 +14,14 @@ public class MusicPlayer {
       GenericPlayer piano = new GenericPlayer(channels, new int[]{0,1,2,3});
       GenericPlayer guitar = new GenericPlayer(channels, new int[]{4,5,6,7});
 
-      piano.playNotesFromChord("CMajor", "C", 100, 1000);
+      piano.playNotesFromChord("CMajor", "C", 100, 3000);
+      guitar.playChord("CMajor", "C", 100, 3000);
+      Thread.sleep(3000);
+      piano.playNotesFromChord("CMajor", "em", 100, 3000);
+      guitar.playChord("CMajor", "em", 100, 3000);
+      Thread.sleep(3000);
+      piano.playNotesFromChord("CMajor", "dm", 100, 3000);
+      guitar.playChord("CMajor", "dm", 100, 3000);
     }
     catch (Exception e) {
       e.printStackTrace();
