fd3818d84a84b8cc21faed6f7eb3015a74d542d5
Brandon Sprague <brandon00sprague@gmail.com>
2015-02-26 23:33:22 -0500

    Scales/chords w/ progresssions

--- danny/Progression.java
+++ danny/Progression.java
@@ -1,12 +1,20 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Progression {
+  private Map<String, Map<String, int[]>> chords;
   private long duration;
   private List<NoteGroup> prog = new ArrayList<NoteGroup>();
   private int len = 0;
 
   public Progression (int duration) {
+    this.duration = (long)duration;
+  }
+
+  public Progression (Map<String, Map<String, int[]>> chords, int duration) {
+    this.chords = chords;
     this.duration = (long)duration;
   }
 
@@ -18,6 +26,18 @@ public class Progression {
 
   // Adding notes of a chord to be played in succession
   public void add(int[] notes, int[] offsets) {
+    for (int offset : offsets) {
+      prog.add(new NoteGroup(notes[offset], offsets.length));
+    }
+    len++;
+  }
+
+  public void add(String scale, String chord, int[] offsets) {
+    if (chords == null) {
+      // You never specified chords, so we're just going to return because
+      // exceptions are gross to handle
+    }
+    int[] notes = chords.get(scale).get(chord);
     for (int offset : offsets) {
       prog.add(new NoteGroup(notes[offset], offsets.length));
     }
