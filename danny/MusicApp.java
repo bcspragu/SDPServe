e4230641608725b8a84e80d6145b3642063821e2
dpbond <bond.daniel.p@gmail.com>
2015-03-02 22:22:18 -0500

    Check the columns with a debug statement

--- danny/MusicApp.java
+++ danny/MusicApp.java
@@ -334,7 +334,7 @@ public class MusicApp {
             }
             chord = mapCellsToNote(numTrueCellsInColumn, key);        //This returns the specific chord as a String within a specified key.
             noteLengthPiano = mapCellsToNoteLength(numTrueCellsInRow);    //Picks a note length used in the progression
-            System.out.println(numTrueCellsInRow);
+            System.out.println(numTrueCellsInColumn);
 
             // if (numTrueCellsInColumn == 0) {
             //     noteLengthPiano = "rest";                           //We want a rest here
