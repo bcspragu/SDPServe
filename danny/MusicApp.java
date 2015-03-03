ca63558512efac302e4190c8bb95432479db5ea9
dpbond <bond.daniel.p@gmail.com>
2015-03-02 20:59:26 -0500

    I'm hopeful this rests when instructed

--- danny/MusicApp.java
+++ danny/MusicApp.java
@@ -68,7 +68,7 @@ public class MusicApp {
                 /*Create Generic Players for Piano and Guitar*/
                 GenericPlayer piano = new GenericPlayer(MidiMaps.tunedMap(), channels, new int[]{0,1,2,3});
                 GenericPlayer guitar = new GenericPlayer(MidiMaps.tunedMap(), channels, new int[]{4,5,6,7});
-                GenericPlayer hihat = new GenericPlayer(MidiMaps.percussionMap(), channels, new int[]{9});		//Many percussion instruments on channel[9] automatically
+                GenericPlayer hihat = new GenericPlayer(MidiMaps.percussionMap(), channels, new int[]{9});      //Many percussion instruments on channel[9] automatically
                 GenericPlayer tomDrum = new GenericPlayer(MidiMaps.percussionMap(), channels, new int[]{8});
                 
                 /*Initialize GridReader*/
@@ -83,14 +83,14 @@ public class MusicApp {
                   pianoGrid = gridStates.getGrid("Piano");
                   guitarGrid = gridStates.getGrid("Guitar");
                 
-                	String key = determineMajorKey();
-                	progs = getProgressions(key, duration);
-                	
-                	piano.play(progs[0], velocity);
-                	guitar.play(progs[1], velocity);
-                	hihat.play(progs[2], velocity);
-                	tomDrum.play(progs[3], velocityTom);
-                	Thread.sleep(duration);
+                    String key = determineMajorKey();
+                    progs = getProgressions(key, duration);
+                    
+                    piano.play(progs[0], velocity);
+                    guitar.play(progs[1], velocity);
+                    hihat.play(progs[2], velocity);
+                    tomDrum.play(progs[3], velocityTom);
+                    Thread.sleep(duration);
               }
                 /*End of Main Program Loop*/
   
@@ -140,15 +140,15 @@ public class MusicApp {
     }
     
     public static Progression[] getProgressions(String key, int duration) {
-    	
-    	Progression[] band = new Progression[4];
-    	Progression[] tunedProgressions = new Progression[2];
-    	tunedProgressions = getTunedProgressions(pianoGrid, guitarGrid, key, duration);
-    	band[0] = tunedProgressions[0];								//Piano
-    	band[1] = tunedProgressions[1];								//Guitar
-    	band[2] = getHiHatProgression(percussionGrid1, duration);	//Hi Hat
-    	band[3] = getTomProgression(percussionGrid2,duration);		//Tom Drum
-    	
+        
+        Progression[] band = new Progression[4];
+        Progression[] tunedProgressions = new Progression[2];
+        tunedProgressions = getTunedProgressions(pianoGrid, guitarGrid, key, duration);
+        band[0] = tunedProgressions[0];                             //Piano
+        band[1] = tunedProgressions[1];                             //Guitar
+        band[2] = getHiHatProgression(percussionGrid1, duration);   //Hi Hat
+        band[3] = getTomProgression(percussionGrid2,duration);      //Tom Drum
+        
         return band;
     }
     
@@ -157,7 +157,7 @@ public class MusicApp {
         
         switch (key) {
         case "CMajor":
-            if (cellCount == 0 || cellCount == 3 || cellCount == 8) {
+            if (cellCount == 3 || cellCount == 8) {
                 chord = "C";
             }
             else if (cellCount == 1) {
@@ -255,66 +255,66 @@ public class MusicApp {
     }
       
     public static String mapCellsToNoteLength(int cellCount) {
-    	String noteType = null;
-    	
-    	switch (cellCount) {
-    	case 0:
-    		noteType = "chord";
-    		break;
-    	case 1:
-    		noteType = "quarter";
-    		break;
-    	case 2:
-    		noteType = "half";
-    		break;
-    	case 3:
-    		noteType = "chord";
-    		break;
-    	case 4:
-    		noteType = "quarter";
-    		break;
-    	case 5:
-    		noteType = "half";
-    		break;
-    	case 6:
-    		noteType = "chord";
-    		break;
-    	case 7:
-    		noteType = "quarter";
-    		break;
-    	case 8:
-    		noteType = "half";
-    		break;
-    	case 9:
-    		noteType = "chord";
-    		break;
-    	case 10:
-    		noteType = "quarter";
-    		break;
-    	case 11:
-    		noteType = "half";
-    		break;
-    	case 12:
-    		noteType = "chord";
-    		break;
-    	default:
-    		noteType = "quarter";
-    		break;
-    	}
-    	
-    	return noteType;
+        String noteType = null;
+        
+        switch (cellCount) {
+        case 0:
+            noteType = "chord";
+            break;
+        case 1:
+            noteType = "quarter";
+            break;
+        case 2:
+            noteType = "half";
+            break;
+        case 3:
+            noteType = "chord";
+            break;
+        case 4:
+            noteType = "quarter";
+            break;
+        case 5:
+            noteType = "half";
+            break;
+        case 6:
+            noteType = "chord";
+            break;
+        case 7:
+            noteType = "quarter";
+            break;
+        case 8:
+            noteType = "half";
+            break;
+        case 9:
+            noteType = "chord";
+            break;
+        case 10:
+            noteType = "quarter";
+            break;
+        case 11:
+            noteType = "half";
+            break;
+        case 12:
+            noteType = "chord";
+            break;
+        default:
+            noteType = "quarter";
+            break;
+        }
+        
+        return noteType;
     }
     
     public static Progression[] getTunedProgressions(boolean[][] grid1, boolean[][] grid2, String key, int duration) {
-    	
-    	int[] quarterNotes = {0,1,2,3};
-    	int[] halfNotes = {0,2};
-    	
-    	/*Make Progression*/
-    	Progression[] tunedProg = new Progression[2];
-    	tunedProg[0] =  new Progression(MidiMaps.tunedMap(), duration);		//Piano
-    	tunedProg[1] =  new Progression(MidiMaps.tunedMap(), duration);		//Guitar
-    	
+        
+        int[] quarterNotes = {0,1,2,3};
+        int[] halfNotes = {0,2};
+        
+        /*Make Progression*/
+        Progression[] tunedProg = new Progression[2];
+        tunedProg[0] =  new Progression(MidiMaps.tunedMap(), duration);     //Piano
+        tunedProg[1] =  new Progression(MidiMaps.tunedMap(), duration);     //Guitar
+        
         int numTrueCellsInColumn = 0;
         int numTrueCellsInRow = 0;
         String chord = null;
@@ -325,56 +325,70 @@ public class MusicApp {
         for (int j = 0; j < columnHeight; j++) {
             for (int i = 0; i < rowLength; i++) {
                 if (grid1[i][j]) {
-                    numTrueCellsInColumn++;							//Count number of true cells in column
+                    numTrueCellsInColumn++;                         //Count number of true cells in column
                 }
                 if (grid1[j][i]) {
-                	numTrueCellsInRow++;							//Count number of true cells in row. Used to determine chord/half/quarter notes
+                    numTrueCellsInRow++;                            //Count number of true cells in row. Used to determine chord/half/quarter notes
                 }
             }
             chord = mapCellsToNote(numTrueCellsInColumn, key);        //This returns the specific chord as a String within a specified key.
-            noteLengthPiano = mapCellsToNoteLength(numTrueCellsInRow);	  //Picks a note length used in the progression
+            noteLengthPiano = mapCellsToNoteLength(numTrueCellsInRow);    //Picks a note length used in the progression
+
+            if (numTrueCellsInColumn == 0) {
+                noteLengthPiano = "rest";                           //We want a rest here
+            }
             
             switch (noteLengthPiano) {
             case "chord":
-            	tunedProg[0].add(key, chord);
-            	break;
+                tunedProg[0].add(key, chord);
+                break;
             case "quarter":
-            	tunedProg[0].add(key, chord, quarterNotes);
-            	break;
+                tunedProg[0].add(key, chord, quarterNotes);
+                break;
             case "half":
-            	tunedProg[0].add(key, chord, halfNotes);
+                tunedProg[0].add(key, chord, halfNotes);
+            case "rest":
+                tunedProg[0].add();
+                break;
             default:
-            	break;
+                break;
             }
             numTrueCellsInColumn = 0;
             numTrueCellsInRow = 0;
         }
         
         /*Parse Guitar Grid*/
-        int numTruePianoCells = 0;									//Used to set chords for guitar
+        int numTruePianoCells = 0;                                  //Used to set chords for guitar
         for (int j = 0; j < columnHeight; j++) {
             for (int i = 0; i < rowLength; i++) {
                 if (grid2[i][j]) {
-                    numTrueCellsInColumn++;							//Count number of true cells in column
+                    numTrueCellsInColumn++;                         //Count number of true cells in column
                 }
                 if (grid1[i][j]) {
-                    numTruePianoCells++;							//Count number of true cells in column
+                    numTruePianoCells++;                            //Count number of true cells in column
                 }
             }
-            noteLengthGuitar = mapCellsToNoteLength(numTrueCellsInColumn);	  //Picks a note length used in the progression
-            chord = mapCellsToNote(numTruePianoCells, key);					  //Pick same chord as piano
+            noteLengthGuitar = mapCellsToNoteLength(numTrueCellsInColumn);    //Picks a note length used in the progression
+            chord = mapCellsToNote(numTruePianoCells, key);                   //Pick same chord as piano
+
+            if (numTrueCellsInColumn == 0) {
+                noteLengthPiano = "rest";                           //We want a rest here
+            }
             
             switch (noteLengthGuitar) {
             case "chord":
-            	tunedProg[1].add(key, chord);
-            	break;
+                tunedProg[1].add(key, chord);
+                break;
             case "quarter":
-            	tunedProg[1].add(key, chord, quarterNotes);
-            	break;
+                tunedProg[1].add(key, chord, quarterNotes);
+                break;
             case "half":
-            	tunedProg[1].add(key, chord, halfNotes);
+                tunedProg[1].add(key, chord, halfNotes);
+            case "rest":
+                tunedProg[1].add();
+                break;
             default:
-            	break;
+                break;
             }
             numTrueCellsInColumn = 0;
             numTruePianoCells = 0;
@@ -384,134 +398,147 @@ public class MusicApp {
     }
     
     public static Progression getHiHatProgression(boolean[][] grid, int duration) {
-    	Progression hiHat = new Progression(MidiMaps.percussionMap(), duration);
-    	
-    	int[] quarterNotes = {0,1,2,3};
-    	int[] halfNotes = {0,2};
-    	int numTrueCellsInColumn = 0;
-    	int numTrueCellsInRow = 0;
-    	String noteLengthHiHat;
-    	String rhythm;
-    	
-    	for (int j = 0; j < columnHeight; j++) {
+        Progression hiHat = new Progression(MidiMaps.percussionMap(), duration);
+        
+        int[] quarterNotes = {0,1,2,3};
+        int[] halfNotes = {0,2};
+        int numTrueCellsInColumn = 0;
+        int numTrueCellsInRow = 0;
+        String noteLengthHiHat;
+        String rhythm;
+        
+        for (int j = 0; j < columnHeight; j++) {
             for (int i = 0; i < rowLength; i++) {
                 if (grid[i][j]) {
-                    numTrueCellsInColumn++;							//Count number of true cells in column
+                    numTrueCellsInColumn++;                         //Count number of true cells in column
                 }
                 if (grid[j][i]) {
-                	numTrueCellsInRow++;							//Count number of true cells in row. Used to determine chord/half/quarter notes
+                    numTrueCellsInRow++;                            //Count number of true cells in row. Used to determine chord/half/quarter notes
                 }
             }
-            noteLengthHiHat = mapCellsToNoteLength(numTrueCellsInRow);	  //Picks a note length used in the progression
+            noteLengthHiHat = mapCellsToNoteLength(numTrueCellsInRow);    //Picks a note length used in the progression
             rhythm = mapCellsToPercussion(numTrueCellsInColumn);
+
+            if (numTrueCellsInColumn == 0) {
+                noteLengthHiHat = "rest";
+            }
             
             switch (noteLengthHiHat) {
             case "chord":
-            	hiHat.add("HiHat", rhythm);
-            	break;
+                hiHat.add("HiHat", rhythm);
+                break;
             case "quarter":
-            	hiHat.add("HiHat", rhythm, quarterNotes);
-            	break;
+                hiHat.add("HiHat", rhythm, quarterNotes);
+                break;
             case "half":
-            	hiHat.add("HiHat", rhythm, halfNotes);
+                hiHat.add("HiHat", rhythm, halfNotes);
+            case "rest":
+                hiHat.add();
+                break;
             default:
-            	break;
+                break;
             }
             numTrueCellsInColumn = 0;
             numTrueCellsInRow = 0;
         }
-    	return hiHat;
+        return hiHat;
     }
     
     public static String mapCellsToPercussion(int cellCount) {
-    	String rhythm;
-    	
-    	switch (cellCount) {
-    	case 0:
-    		rhythm = "CCCC";
-    		break;
-    	case 1:
-    		rhythm = "OCOC";
-    		break;
-    	case 2:
-    		rhythm = "COCO";
-    		break;
-    	case 3:
-    		rhythm = "CCOO";
-    		break;
-    	case 4:
-    		rhythm = "OOCC";
-    		break;
-    	case 5:
-    		rhythm = "OOOO";
-    		break;
-    	case 6:
-    		rhythm = "CCCC";
-    		break;
-    	case 7:
-    		rhythm = "OCOC";
-    		break;
-    	case 8:
-    		rhythm = "COCO";
-    		break;
-    	case 9:
-    		rhythm = "CCOO";
-    		break;
-    	case 10:
-    		rhythm = "OOCC";
-    		break;
-    	case 11:
-    		rhythm = "OOOO";
-    		break;
-    	case 12:
-    		rhythm = "CCCC";
-    		break;
-    	default:
-    		rhythm = "COCO";
-    		break;
-    	}
-    	
-    	return rhythm;
+        String rhythm;
+        
+        switch (cellCount) {
+        case 0:
+            rhythm = "CCCC";
+            break;
+        case 1:
+            rhythm = "OCOC";
+            break;
+        case 2:
+            rhythm = "COCO";
+            break;
+        case 3:
+            rhythm = "CCOO";
+            break;
+        case 4:
+            rhythm = "OOCC";
+            break;
+        case 5:
+            rhythm = "OOOO";
+            break;
+        case 6:
+            rhythm = "CCCC";
+            break;
+        case 7:
+            rhythm = "OCOC";
+            break;
+        case 8:
+            rhythm = "COCO";
+            break;
+        case 9:
+            rhythm = "CCOO";
+            break;
+        case 10:
+            rhythm = "OOCC";
+            break;
+        case 11:
+            rhythm = "OOOO";
+            break;
+        case 12:
+            rhythm = "CCCC";
+            break;
+        default:
+            rhythm = "COCO";
+            break;
+        }
+        
+        return rhythm;
     }
       
     public static Progression getTomProgression(boolean[][] grid, int duration) {
-    	Progression tom = new Progression(MidiMaps.percussionMap(), duration);
-    	
-    	int[] quarterNotes = {0,1,2,3};
-    	int[] halfNotes = {0,2};
-    	int numTrueCellsInColumn = 0;
-    	int numTrueCellsInRow = 0;
-    	String noteLengthTom;
-    	String rhythm;
-    	
-    	for (int j = 0; j < columnHeight; j++) {
+        Progression tom = new Progression(MidiMaps.percussionMap(), duration);
+        
+        int[] quarterNotes = {0,1,2,3};
+        int[] halfNotes = {0,2};
+        int numTrueCellsInColumn = 0;
+        int numTrueCellsInRow = 0;
+        String noteLengthTom;
+        String rhythm;
+        
+        for (int j = 0; j < columnHeight; j++) {
             for (int i = 0; i < rowLength; i++) {
                 if (grid[i][j]) {
-                    numTrueCellsInColumn++;							//Count number of true cells in column
+                    numTrueCellsInColumn++;                         //Count number of true cells in column
                 }
                 if (grid[j][i]) {
-                	numTrueCellsInRow++;							//Count number of true cells in row. Used to determine chord/half/quarter notes
+                    numTrueCellsInRow++;                            //Count number of true cells in row. Used to determine chord/half/quarter notes
                 }
             }
-            noteLengthTom = mapCellsToNoteLength(numTrueCellsInRow);	  //Picks a note length used in the progression
+            noteLengthTom = mapCellsToNoteLength(numTrueCellsInRow);      //Picks a note length used in the progression
             rhythm = mapCellsToPercussion(numTrueCellsInColumn);
+
+            if (numTrueCellsInColumn == 0) {
+                noteLengthTom = "rest";
+            }
             
             switch (noteLengthTom) {
             case "chord":
-            	tom.add("TomDrum", rhythm);
-            	break;
+                tom.add("TomDrum", rhythm);
+                break;
             case "quarter":
-            	tom.add("TomDrum", rhythm, quarterNotes);
-            	break;
+                tom.add("TomDrum", rhythm, quarterNotes);
+                break;
             case "half":
-            	tom.add("TomDrum", rhythm, halfNotes);
+                tom.add("TomDrum", rhythm, halfNotes);
+            case "rest":
+                tom.add();
+                break;
             default:
-            	break;
+                break;
             }
             numTrueCellsInColumn = 0;
             numTrueCellsInRow = 0;
         }
-    	return tom;
+        return tom;
     }
 }
-
