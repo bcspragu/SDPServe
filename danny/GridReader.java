b4af2dc413d58d6bf20a7d1f258d76489e30d9ee
dpbond <bond.daniel.p@gmail.com>
2015-04-14 11:33:47 -0400

    enable localhost for URL

--- danny/GridReader.java
+++ danny/GridReader.java
@@ -34,8 +34,8 @@ public class GridReader {
 
   public static Grids getCurrentState() {
     try {
-      //JSONObject data = readJsonFromUrl("http://localhost:8080/grids.json");
-      JSONObject data = readJsonFromUrl("http://192.168.2.1:8080/grids.json");
+      JSONObject data = readJsonFromUrl("http://localhost:8080/grids.json");
+      //JSONObject data = readJsonFromUrl("http://192.168.2.1:8080/grids.json");
       return new Grids(data);
     } catch (IOException e) {
       return new Grids(new JSONObject());
@@ -44,8 +44,8 @@ public class GridReader {
 
   public static Management getMgmt() {
     try {
-      //JSONObject data = readJsonFromUrl("http://localhost:8080/mgmt.json");
-      JSONObject data = readJsonFromUrl("http://192.168.2.1:8080/mgmt.json");
+      JSONObject data = readJsonFromUrl("http://localhost:8080/mgmt.json");
+      //JSONObject data = readJsonFromUrl("http://192.168.2.1:8080/mgmt.json");
       return new Management(data);
     } catch (IOException e) {
       return new Management(new JSONObject());
